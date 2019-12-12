1abff298c0008ae75f5bb17a9f23ec4ae3faa823
igonzaleziglesias <43774705+igonzaleziglesias@users.noreply.github.com>
2019-12-12 10:12:45 +0100

    comentado

--- Rotator.cs
+++ Rotator.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
-    void Update () 
+    void Update () //anima los pick ups haciendolos rotar con un inclinacion
     {
         transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
     }
